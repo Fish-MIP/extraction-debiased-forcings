185da1e87800ab0a62f738105d8577a5b9ecef1e
barrier.n <nicolas.barrier@ird.fr>
2026-03-24 14:01:57 +0000

    Adding scripts to extract thkcello and wo

---
+++ extract_wo.py
@@ -0,0 +1,86 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python [conda env:nbarrier2]
+#     language: python
+#     name: conda-env-nbarrier2-py
+# ---
+
+# %%
+import xarray as xr
+from glob import glob
+import os
+import numpy as np
+
+# %%
+mesh = xr.open_dataset("/home1/datawork/nbarrier/apecosm/apecosm-private/test/resources/mesh_mask_orca1.nc4")
+mesh = mesh.squeeze()
+mesh
+
+# %%
+# mbathy returns the number of ocean cells.
+# if 74, to have the bottom cell we must extract at k=73
+# so we remove 1 to mbathy. When it is land, mbathy is then -1 and we 
+# reset negative values to 0
+mbathy = mesh['mbathy'] - 1
+mbathy = mbathy.where(mbathy >= 0).fillna(0)
+mbathy = mbathy.astype(int)
+mbathy
+
+# %%
+for scenario in ['SSP245',  'SSP370',  'SSP585', 'SSP126']:
+
+    print("-------------------------- Processing scenario ", scenario)
+
+    # Output folder
+    dirout = os.path.join('/home1/scratch/nbarrier/fishmip-osp/velocities', scenario.lower())
+    dirout
+    
+    # Create output folder if not exists
+    if not os.path.exists(dirout):
+        os.makedirs(dirout)
+    
+    # Extract the list of forcing files (one file per month) for the given scenario
+    
+    dirin = os.path.join('/home/datawork-marbec-scenlab/NEMO/FORCING-FISHMIP/', f'{scenario}-fIPSL-cOBSN-v2', 'Output')
+    dirin
+    
+    filelist = glob(os.path.join(dirin, '*v2_20[2-9]*1m*grid_W*'))
+    filelist += glob(os.path.join(dirin, '*v2_201[5-9]*1m*grid_W*'))
+    filelist.sort()
+    # print(filelist)
+
+    for f in filelist:
+
+        print("+++++ Processing file ", f)
+
+        try:
+            data = xr.open_dataset(f)
+        except:
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@ error with ", f)
+            continue
+        data = data.rename({"time_counter": "time"})
+
+        # Conversion from C to K
+        temp = (data['wo'])
+        temp.name = 'wo'
+        temp.attrs['units'] = 'm/s'
+
+        date = temp['time']
+        date
+        
+        years = np.array([d.year for d in date.values])
+        months = np.array([d.month for d in date.values])
+        days = np.array([d.day for d in date.values])
+
+        foutname = os.path.join(dirout, f'ipsl_{scenario.lower()}_{temp.name}_1deg_global_monthly_{years.min()}_{years.max()}.nc')
+        temp.to_netcdf(foutname, unlimited_dims=['time'])
+
+# %%
