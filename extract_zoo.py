9c87affd207ac41754b26b9ae113bbda11908ee9
barrier.n <nicolas.barrier@ird.fr>
2026-03-24 13:06:00 +0000

    Update extraction of zoo

--- extract_zoo.py
+++ extract_zoo.py
@@ -32,7 +32,7 @@ for scenario in ['SSP245',  'SSP370',  'SSP585', 'SSP126']:
     print("-------------------------- Processing scenario ", scenario)
 
     # Output folder
-    dirout = os.path.join('/home1/scratch/nbarrier/fishmip-osp/', scenario.lower())
+    dirout = os.path.join('/home1/scratch/nbarrier/fishmip-osp/phyto', scenario.lower())
     dirout
     
     # Create output folder if not exists
@@ -55,32 +55,61 @@ for scenario in ['SSP245',  'SSP370',  'SSP585', 'SSP126']:
         except:
             print("@@@@@@@@@@@@@@@@@@@@@@@@@@@ error with ", f)
             continue
-        data = data.rename({"time_counter": "time"})
+        data = data.rename({"time_counter": "time", "olevel": 'z'})
 
-        szoo = mmol_to_mol * data['ZOO']
-        szoo.name = 'szoo'
-        szoo.attrs['units'] = 'mol/m3'
+        #-------- processing diatoms
+        zmicro = mmol_to_mol * data['ZOO']
+        zmicro.name = 'zmicro'
+        zmicro.attrs['units'] = 'mol/m3'
+
+        zmicro_vint = zmicro.weighted(weight).sum(dim='z')
+        zmicro_vint.name = 'zmicro-vint'
+        zmicro_vint.attrs['units'] = 'mol/m2'
+
+        #--------- processing misc
+        zmeso = mmol_to_mol * data['ZOO2']
+        zmeso.name = 'zmeso'
+        zmeso.attrs['units'] = 'mol/m3'
+
+        zmeso_vint = zmeso.weighted(weight).sum(dim='z')
+        zmeso_vint.name = 'zmeso-vint'
+        zmeso_vint.attrs['units'] = 'mol/m2'
+
+        #------- Sum of diat + misc
+        zooc = zmicro + zmeso
+        zooc.name = 'zooc'
+        zooc.attrs['units'] = 'mol/m3'
+
+        zooc_vint = zooc.weighted(weight).sum(dim='z')
+        zooc_vint.name = 'zooc-vint'
+        zooc_vint.attrs['units'] = 'mol/m2'
         
-        lzoo = mmol_to_mol * data['ZOO2']
-        lzoo.name = 'lzoo'
-        lzoo.attrs['units'] = 'mol/m3'
-        
-        zoo = szoo + lzoo
-        zoo.name = 'zoo'
-        zoo.attrs['units'] = 'mol/m3'
-        
-        date = zoo['time']
+        date = zooc['time']
         date
         
         years = np.array([d.year for d in date.values])
         months = np.array([d.month for d in date.values])
         days = np.array([d.day for d in date.values])
 
-        for var in [szoo, lzoo, zoo]: 
-            print(var.name)
-            foutname = os.path.join(dirout, f'ipsl_{scenario.lower()}_{var.name}_1deg_global_monthly_{years.min()}_{years.max()}.nc')
-            foutname
-            print(foutname)
-            var.to_netcdf(foutname, unlimited_dims=['time'])
+        foutname = os.path.join(dirout, f'ipsl_{scenario.lower()}_zmicro_1deg_global_monthly_{years.min()}_{years.max()}.nc')
+        foutname
+        dsout = xr.Dataset()
+        dsout['zmicro'] = zmicro
+        dsout['zmicro-vint'] = zmicro_vint
+        dsout.to_netcdf(foutname, unlimited_dims=['time'])   
+
+        foutname = os.path.join(dirout, f'ipsl_{scenario.lower()}_zmeso_1deg_global_monthly_{years.min()}_{years.max()}.nc')
+        foutname
+        dsout = xr.Dataset()
+        dsout['zmeso'] = zmeso
+        dsout['zmeso-vint'] = zmeso_vint
+        dsout.to_netcdf(foutname, unlimited_dims=['time'])   
+
+        foutname = os.path.join(dirout, f'ipsl_{scenario.lower()}_zooc_1deg_global_monthly_{years.min()}_{years.max()}.nc')
+        foutname
+        dsout = xr.Dataset()
+        dsout['zooc'] = zooc
+        dsout['zooc-vint'] = zooc_vint
+        dsout.to_netcdf(foutname, unlimited_dims=['time'])   
 
 # %%
