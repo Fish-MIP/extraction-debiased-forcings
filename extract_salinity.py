b32c12da9873b8cf24cd130ca2d73a8ef07b90ec
barrier.n <nicolas.barrier@ird.fr>
2026-03-24 13:08:56 +0000

    Update script for salinity extraction

--- extract_salinity.py
+++ extract_salinity.py
@@ -21,7 +21,17 @@ import numpy as np
 
 # %%
 mesh = xr.open_dataset("/home1/datawork/nbarrier/apecosm/apecosm-private/test/resources/mesh_mask_orca1.nc4")
-mesh
+mesh = mesh.squeeze()
+
+# %%
+# mbathy returns the number of ocean cells.
+# if 74, to have the bottom cell we must extract at k=73
+# so we remove 1 to mbathy. When it is land, mbathy is then -1 and we 
+# reset negative values to 0
+mbathy = mesh['mbathy'] - 1
+mbathy = mbathy.where(mbathy >= 0).fillna(0)
+mbathy = mbathy.astype(int)
+mbathy
 
 # %%
 for scenario in ['SSP245',  'SSP370',  'SSP585', 'SSP126']:
@@ -60,6 +70,23 @@ for scenario in ['SSP245',  'SSP370',  'SSP585', 'SSP126']:
         salt = (data['so'])
         salt.name = 'so'
 
+        # creation of salt_surf variable by taking the 
+        # first ocean level
+        salt_surf = salt.isel(olevel=0)
+        salt_surf.name = 'so-surf'
+        salt_surf.attrs['units'] = salt.attrs['units']
+    
+        # creation of salt_bot variable by taking the 
+        # last ocean level
+        salt_bot = salt.isel(olevel=mbathy)
+        salt_bot.name = 'so-bot'
+        salt_bot.attrs['units'] = salt.attrs['units']
+        
+        dsout = xr.Dataset()
+        dsout['so'] = salt
+        dsout['so-bot'] = salt_bot
+        dsout['so-surf'] = salt_surf
+
         date = salt['time']
         date
         
@@ -67,7 +94,7 @@ for scenario in ['SSP245',  'SSP370',  'SSP585', 'SSP126']:
         months = np.array([d.month for d in date.values])
         days = np.array([d.day for d in date.values])
 
-        foutname = os.path.join(dirout, f'ipsl_{scenario.lower()}_{salt.name}_1deg_global_monthly_{years.min()}_{years.max()}.nc')
+        foutname = os.path.join(dirout, f'ipsl_{scenario.lower()}_so_1deg_global_monthly_{years.min()}_{years.max()}.nc')
         foutname
         print(foutname)
         salt.to_netcdf(foutname, unlimited_dims=['time'])
